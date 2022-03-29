627ead82749e27bf45f3f95396469feb452ef145
Edwin <edwinthomasg29@gmail.com>
2022-03-29 08:56:49 +0530

    commit

--- Arrow/arrow.js
+++ Arrow/arrow.js
@@ -7,3 +7,10 @@
 const square = (num) => num*num; //concised way of coding 
 
 console.log("square result = "+square(5));
+
+const arr = [1,2,3,4,"edwin"]
+arr.forEach((value)=>{
+    console.log(value)
+})
+for(let value of arr)
+{console.log(value)}
--- Iterators and Generators/generators.js
+++ Iterators and Generators/generators.js
@@ -2,8 +2,10 @@ function *display(){
     yield 1;
     console.log("1st yield")
     yield 2;
+    yield 3;
 }
 const ob = display();
+console.log(ob.next())
 console.log(ob.next())
 console.log(ob.next())
 console.log(ob.next())
